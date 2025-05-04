274c2e9d054ac2c2e7290c809c809b74249a43f8
Paweł Kruszko <pawelkruszko@gmail.com>
2025-05-04 18:29:29 +0200

    user model upd and tests added

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="John", email="john@email.com", password="testpassword12345"
+        )
+        self.assertEqual(user.username, "John")
+        self.assertEqual(user.email, "john@email.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        AdminUser = User.objects.create_superuser(
+            username="Admin", email="admin@email.com", password="testpassword12345"
+        )
+        self.assertEqual(AdminUser.username, "Admin")
+        self.assertEqual(AdminUser.email, "admin@email.com")
+        self.assertTrue(AdminUser.is_active)
+        self.assertTrue(AdminUser.is_staff)
+        self.assertTrue(AdminUser.is_superuser)
